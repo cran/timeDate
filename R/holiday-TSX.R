8ea1082858ad470dae1ae9842f3ae4204f619bb8
Abhinav Jindal <abh.jindal@yahoo.com>
2017-08-31 15:42:27 -0400

    Update holiday-TSX.R

--- R/holiday-TSX.R
+++ R/holiday-TSX.R
@@ -79,6 +79,7 @@ holidayTSX <-
         CAVictoriaDay(year),
         CACanadaDay(year),
         CACivicProvincialHoliday(year),
+        CALabourDay(year),
         CAThanksgivingDay(year),
         ChristmasDay(year),
         BoxingDay(year))
